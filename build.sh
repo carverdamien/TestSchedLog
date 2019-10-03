22ad6ab1b422227aa93715c1c9e4b69fd93a8b38
Damien Carver <carverdamien@gmail.com>
2019-10-03 16:12:43 +0200

    mv IMG= in img/names

--- build.sh
+++ build.sh
@@ -4,7 +4,6 @@ set -e -x
 git submodule init
 git submodule sync --recursive
 git submodule update --recursive --remote
-export KERNEL_IMG="testschedlog-kernel:$(cd img/kernel/src; git rev-parse HEAD)"
+. img/names
 (cd img/kernel; ./build.sh)
-export QEMU_IMG="testschedlog-qemu:$(git rev-parse HEAD)"
 (cd img/qemu; ./build.sh)
