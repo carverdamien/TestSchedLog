6f6a1bacf679d18eb0f54b87922ad738211cb7e5
Damien Carver <carverdamien@gmail.com>
2019-10-04 14:19:12 +0200

    silent

--- tests/sched_log_tracer_logs.test.sh
+++ tests/sched_log_tracer_logs.test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo 1 | tee /sys/kernel/debug/sched_log/tracer/{enable,events/*}
-cat /sys/kernel/debug/sched_log/tracer/logs/*
+cat /sys/kernel/debug/sched_log/tracer/logs/* > /dev/null
